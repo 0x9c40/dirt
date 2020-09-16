09eeef9f432120e6f35f7fd13cd5fe3091d01488
Lubomir Vinnik <lubomirvinnik@gmail.com>
2020-09-16 15:34:11 +0300

    Add an example of split2 npm package usage.
    mini line-count program, to check if it correct run:
    $ node line-counter.js < line-counter.js
    $ wc -l < line-counter.js

---
+++ node/split/line-counter.js
@@ -0,0 +1,14 @@
+const split = require("split2");
+const through = require("through2");
+
+let count = 0;
+process.stdin.pipe(split()).pipe(through(write, end));
+
+function write(buf, enc, next) {
+  count++;
+  next();
+}
+
+function end() {
+  console.log(count);
+}
